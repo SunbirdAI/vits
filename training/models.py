febf0c74940792b22227984bc0939affb7cb3559
mekaneeky <alizawahry1@gmail.com>
2023-10-09 13:56:58 +0300

    relative import fix

--- training/models.py
+++ training/models.py
@@ -11,7 +11,7 @@ from . import monotonic_align
 
 from torch.nn import Conv1d, ConvTranspose1d, AvgPool1d, Conv2d
 from torch.nn.utils import weight_norm, remove_weight_norm, spectral_norm
-from commons import init_weights, get_padding
+from .commons import init_weights, get_padding
 
 
 class StochasticDurationPredictor(nn.Module):
